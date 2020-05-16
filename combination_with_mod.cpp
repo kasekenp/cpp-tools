163b1ad7d06e92ce78de926514b2c782ae90b932
kaseken <kento.kaseda@playground.live>
2020-05-16 22:47:05 +0900

    feat: add combination_with_mod.cpp

---
+++ combination_with_mod.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAX = 100010;
+const int MOD = 1000000007;
+
+int64_t fac[MAX], finv[MAX], inv[MAX];
+
+void COMinit()
+{
+  fac[0] = fac[1] = 1;
+  finv[0] = finv[1] = 1;
+  inv[1] = 1;
+  for (int i = 2; i < MAX; i++)
+  {
+    fac[i] = fac[i - 1] * i % MOD;
+    inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+    finv[i] = finv[i - 1] * inv[i] % MOD;
+  }
+}
+
+int64_t COM(int n, int k)
+{
+  if (n < k)
+    return 0;
+  if (n < 0 || k < 0)
+    return 0;
+  return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
