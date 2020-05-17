5078e49660240a12b04aeb8bf668258bfbe12e72
kaseken <kento.kaseda@playground.live>
2020-05-17 16:56:10 +0900

    fix: update power_mod.cpp

--- power_mod.cpp
+++ power_mod.cpp
@@ -2,23 +2,18 @@
 using namespace std;
 const int64_t MOD = 1000000007;
 
-int64_t pow_mod(int64_t M, int64_t N)
+int64_t pow_mod(int64_t x, int64_t n)
 {
-  int64_t POW[100];
-  POW[0] = 1;
-  POW[1] = M;
-  for (int i = 2; i < 100; i++)
-    POW[i] = (POW[i - 1] * POW[i - 1]) % MOD;
-  int64_t bit = N, k = 1, m = 1;
-  while (N > 0)
+  int64_t res = 1;
+  if (n > 0)
   {
-    int a = N % 2;
-    if (a)
-      m = (m * POW[k]) % MOD;
-    k++;
-    N /= 2;
+    res = pow_mod(x, n / 2);
+    if (n % 2 == 0)
+      res = (res * res) % MOD;
+    else
+      res = (((res * res) % MOD) * x) % MOD;
   }
-  return m;
+  return res;
 }
 
 int main()
