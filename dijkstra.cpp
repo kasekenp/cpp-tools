e51b0c80aaf964c7e0b22a6f4cc4371d6aea08cd
kaseken <kento.kaseda@playground.live>
2020-05-23 18:36:57 +0900

    fix: update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,41 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-int64_t INF = (1 << 20);
-int64_t N, MAXN = 10010;
-vector<vector<pair<int64_t, int64_t>>> Edges(MAXN);
+int64_t INF = (1LL << 62);
+int64_t N, MAXN = 100010;
+vector<vector<pair<int, int>>> Graph(MAXN);
 
-void dijkstra()
+int64_t dijkstra(int64_t start, int64_t goal)
 {
   priority_queue<pair<int64_t, int64_t>, vector<pair<int64_t, int64_t>>, greater<pair<int64_t, int64_t>>> PQ;
-  vector<int64_t> Color(MAXN, 0), D(MAXN, INF); // 各点を無限大にする。
+  vector<int64_t> Color(MAXN, 0), D(MAXN, INF);
 
-  D.at(0) = 0; // 始点を0とする。
-  PQ.push(make_pair(0, 0));
-  Color.at(0) = 1; // グレーの状態に
+  D.at(start) = 0;
+  PQ.push(make_pair(0, start));
+  Color.at(start) = 1;
 
   while (!PQ.empty())
   {
     pair<int64_t, int64_t> f = PQ.top();
     PQ.pop();
-    int64_t u = f.second; // 未決定の中で、最短経路の頂点
-    Color.at(u) = 2;      // 決定済とする
+    int64_t u = f.second;
+    Color.at(u) = 2;
     if (D.at(u) < f.first)
       continue;
-    for (int i = 0; i < Edges.at(u).size(); i++)
+    for (int i = 0; i < Graph.at(u).size(); i++)
     {
-      int64_t v = Edges.at(u).at(i).first; // uからの辺の終点
+      int64_t v = Graph.at(u).at(i).first;
       if (Color.at(v) == 2)
         continue;
-      if (D.at(v) > D.at(u) + Edges.at(u).at(i).second) // uを経由した場合の距離
+      if (D.at(v) > D.at(u) + Graph.at(u).at(i).second)
       {
-        D.at(v) = D.at(u) + Edges.at(u).at(i).second;
-        PQ.push(make_pair(D.at(v), v)); // 頂点vの暫定での距離、頂点vをペアにする
-        Color.at(v) = 1;                // グレーにする
+        D.at(v) = D.at(u) + Graph.at(u).at(i).second;
+        PQ.push(make_pair(D.at(v), v));
+        Color.at(v) = 1;
       }
     }
   }
-  for (int i = 0; i < N; i++)
-    cout << i << ' ' << (D.at(i) == INF ? -1 : D.at(i)) << endl;
+  return D.at(goal);
 }
 
 int main()
