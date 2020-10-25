5937133725a423fb30866d1764d447b400f8ef30
kaseken <kento.kaseda@playground.live>
2020-10-25 21:20:35 +0900

    fix: update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,55 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-int64_t INF = (1LL << 62);
-int64_t N, MAXN = 100010;
-vector<vector<pair<int, int>>> Graph(MAXN);
+typedef pair<long long, long long> P;
+const long long INF = (1LL << 62);
+const int MAXV = 100010;
+long long V, E, r;
+vector<P> to[MAXV];
 
-int64_t dijkstra(int64_t start, int64_t goal)
+void dijkstra(int start)
 {
-  priority_queue<pair<int64_t, int64_t>, vector<pair<int64_t, int64_t>>, greater<pair<int64_t, int64_t>>> PQ;
-  vector<int64_t> Color(MAXN, 0), D(MAXN, INF);
+  priority_queue<P, vector<P>, greater<P>> PQ;
+  vector<long long> D(V, INF);
+  vector<bool> visited(V, false);
 
   D.at(start) = 0;
-  PQ.push(make_pair(0, start));
-  Color.at(start) = 1;
-
+  PQ.emplace(0, start);
   while (!PQ.empty())
   {
-    pair<int64_t, int64_t> f = PQ.top();
+    long long dv, v;
+    tie(dv, v) = PQ.top();
     PQ.pop();
-    int64_t u = f.second;
-    Color.at(u) = 2;
-    if (D.at(u) < f.first)
-      continue;
-    for (int i = 0; i < Graph.at(u).size(); i++)
+    visited.at(v) = true;
+    if (D.at(v) < dv)
     {
-      int64_t v = Graph.at(u).at(i).first;
-      if (Color.at(v) == 2)
-        continue;
-      if (D.at(v) > D.at(u) + Graph.at(u).at(i).second)
+      continue;
+    }
+    for (auto p : to[v])
+    {
+      long long u, du;
+      tie(u, du) = p;
+      if (visited.at(u))
       {
-        D.at(v) = D.at(u) + Graph.at(u).at(i).second;
-        PQ.push(make_pair(D.at(v), v));
-        Color.at(v) = 1;
+        continue;
+      }
+      if (D.at(u) > D.at(v) + du)
+      {
+        D.at(u) = D.at(v) + du;
+        PQ.emplace(D.at(u), u);
       }
     }
   }
-  return D.at(goal);
+
+  for (auto d : D)
+  {
+    cout << (d == INF ? "INF" : to_string(d)) << endl;
+  }
 }
 
 int main()
 {
-  cin >> N;
-  int k, u, v, c;
-  for (int i = 0; i < N; i++)
+  cin >> V >> E >> r;
+  for (int i = 0; i < E; i++)
   {
-    cin >> u >> k;
-    for (int j = 0; j < k; j++)
-    {
-      cin >> v >> c;
-      Edges.at(u).push_back(make_pair(v, c));
-    }
+    long long s, t, d;
+    cin >> s >> t >> d;
+    to[s].emplace_back(t, d);
   }
-
-  dijkstra();
+  dijkstra(r);
 }
