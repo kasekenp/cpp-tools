ee98f2af67e9f0251d097139aae54209f7009266
kaseken <k.kaseda0925@gmail.com>
2021-06-02 09:16:00 +0900

    fix: update CombinationWithMod.cpp

--- cpp/CombinationWithMod.cpp
+++ cpp/CombinationWithMod.cpp
@@ -1,28 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 100010;
-const int64_t MOD = 1000000007;
 
-int64_t fac[MAX], finv[MAX], inv[MAX];
+class Combination {
+   private:
+    static const int MAX = 100010;
+    static const long long MOD = 1000000007;
+    long long fac[MAX], finv[MAX], inv[MAX];
 
-void initCombination()
-{
-  fac[0] = fac[1] = 1;
-  finv[0] = finv[1] = 1;
-  inv[1] = 1;
-  for (int i = 2; i < MAX; i++)
-  {
-    fac[i] = fac[i - 1] * i % MOD;
-    inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
-    finv[i] = finv[i - 1] * inv[i] % MOD;
-  }
-}
+   public:
+    Combination() {
+        fac[0] = fac[1] = 1;
+        finv[0] = finv[1] = 1;
+        inv[1] = 1;
+        for (int i = 2; i < MAX; i++) {
+            fac[i] = fac[i - 1] * i % MOD;
+            inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+            finv[i] = finv[i - 1] * inv[i] % MOD;
+        }
+    }
 
-int64_t nCk(int n, int k)
-{
-  if (n < k)
-    return 0;
-  if (n < 0 || k < 0)
-    return 0;
-  return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
-}
+    long long nCk(int n, int k) {
+        if (n < k) return 0;
+        if (n < 0 || k < 0) return 0;
+        return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+    }
+
+    long long nHk(int n, int k) { return nCk(n + k - 1, k); }
+};
