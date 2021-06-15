73ea33c44dc3f87624ceeec1c25f2634220f9ec0
kaseken <k.kaseda0925@gmail.com>
2021-06-16 00:13:31 +0900

    fix: update Dijkstra.cpp

--- cpp/Dijkstra.cpp
+++ cpp/Dijkstra.cpp
@@ -1,53 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<long long, long long> P;
-const long long INF = (1LL << 62);
-const int MAXV = 100010;
-long long V, E, r;
-vector<P> to[MAXV];
 
-void dijkstra(int start)
-{
-  priority_queue<P, vector<P>, greater<P>> PQ;
-  vector<long long> D(V, INF);
-
-  D.at(start) = 0;
-  PQ.emplace(0, start);
-  while (!PQ.empty())
-  {
-    long long dv, v;
-    tie(dv, v) = PQ.top();
-    PQ.pop();
-    if (D.at(v) < dv)
-    {
-      continue;
+class Dijkstra {
+   public:
+    typedef pair<long long, long long> P;
+    const long long INF = (1LL << 62);
+    vector<long long> min_path(vector<vector<P>> &to, int start) {
+        priority_queue<P, vector<P>, greater<P>> pq;
+        vector<long long> d((int)to.size(), INF);
+        d.at(start) = 0;
+        pq.emplace(0, start);
+        while (!pq.empty()) {
+            long long dv, v;
+            tie(dv, v) = pq.top();
+            pq.pop();
+            if (d.at(v) < dv) {
+                continue;
+            }
+            for (auto p : to.at(v)) {
+                long long u, du;
+                tie(u, du) = p;
+                if (d.at(u) > d.at(v) + du) {
+                    d.at(u) = d.at(v) + du;
+                    pq.emplace(d.at(u), u);
+                }
+            }
+        }
+        return d;
     }
-    for (auto p : to[v])
-    {
-      long long u, du;
-      tie(u, du) = p;
-      if (D.at(u) > D.at(v) + du)
-      {
-        D.at(u) = D.at(v) + du;
-        PQ.emplace(D.at(u), u);
-      }
+};
+
+int main() {
+    int V, E, r;
+    cin >> V >> E >> r;
+    vector<vector<Dijkstra::P>> to(V);
+    for (int i = 0; i < E; i++) {
+        long long s, t, d;
+        cin >> s >> t >> d;
+        to[s].emplace_back(t, d);
     }
-  }
-
-  for (auto d : D)
-  {
-    cout << (d == INF ? "INF" : to_string(d)) << endl;
-  }
-}
-
-int main()
-{
-  cin >> V >> E >> r;
-  for (int i = 0; i < E; i++)
-  {
-    long long s, t, d;
-    cin >> s >> t >> d;
-    to[s].emplace_back(t, d);
-  }
-  dijkstra(r);
+    Dijkstra dijkstra;
+    auto res = dijkstra.min_path(to, r);
+    for (const auto n : res) {
+        cout << (n == Dijkstra::INF ? "INF" : to_string(n)) << endl;
+    }
 }
